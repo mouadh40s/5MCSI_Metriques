5e4abd699bf81b92407c1e23d4d0701ea249c970
mouadh40s <69018468+mouadh40s@users.noreply.github.com>
2024-02-12 15:15:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,7 @@ def extract_minutes(date_string):
         date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
         minutes = date_object.minute
         return jsonify({'minutes': minutes})
+  return render_template('commits')
                                                                                                                                        
 @app.route('/')
 def hello_world():
