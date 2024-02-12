0c2bafada8937dfec51606b9aaa58fab0321873a
mouadh40s <69018468+mouadh40s@users.noreply.github.com>
2024-02-12 16:42:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,12 +30,6 @@ def mongraphique():
 def monhistogramme():
     return render_template("histogramme.html")
 
-@app.route('/extract-minutes/<date_string>')
-git add DateExtract.py
-def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
                                                                                                                                        
 @app.route('/')
 def hello_world():
